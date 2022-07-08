a802ba01ad3575fa33427a5f9c210c3e570ada4c
fatime4 <102423500+fatime4@users.noreply.github.com>
2022-07-08 16:57:39 +0300

    hw-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
+'use strict';
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBoxEl = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems.map(el => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`;
+});
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryItemsEl.join(''));
+
+const imageClickEvent = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largePicture = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largePicture}" width="800" height="600">
+`);
+
+  instance.show();
+
+  window.addEventListener('keydown', onEscBtnPush);
+
+  function onEscBtnPush(event) {
+    if (event.code !== 'Escape') {
+      return;
+    }
+    instance.close();
+  }
+};
+galleryBoxEl.addEventListener('click', imageClickEvent);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryBoxEl = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems.map(el => {
+  return `
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>`;
+});
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryItemsEl.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
